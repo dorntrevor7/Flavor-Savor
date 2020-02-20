a2e2f66d366e0acaf94dc094f864019a344ee3c1
Megan Coppins <meganlcoppins@gmail.com>
2020-02-20 12:02:05 -0700

    input form commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,12 @@
 var recipeID = "f918aaaf";
 var recipeKEY = "8b9bf08b57c94197c63717a122f611d7";
-search = $(".input").val();
 
 // on click event for search button
-$("#searchBtn").on("click", function(e){
+$("#searchBtn").on("click", function(event){
+    event.preventDefault();
+    var search = $("#search").val();
+    console.log(search);
+
     var recipeURL = "https://api.edamam.com/search?app_key=" + recipeKEY + "&app_id=" + recipeID + "&q=" + search + "&to=18"; 
 
 $.ajax({
@@ -12,20 +15,11 @@ $.ajax({
     
 }).then(function(response){
 
-    console.log(response.hits[0].recipe.label);
-    console.log(response.hits[0].recipe.image);
-    console.log(response.hits[0].recipe.url);
-    console.log(response.hits[0].recipe.yield);
-
-    var label=  $("<h3>").text(response.hits[0].recipe.label);
-    var url = $("<a>").attr("href", response.hits[0].recipe.url).text("Go To Recipe");
-    var yields = $("<h5>").text("Yields: " + response.hits[0].recipe.yield);
+    // var label=  $("<h3>").text(response.hits[0].recipe.label);
+    var url = $("<a>").attr("href", response.hits[0].recipe.url).text(response.hits[0].recipe.label)
     var image = $("<img>").attr("src", response.hits[0].recipe.image);
 
 // to link and append to carousel
-        $("#").append(label);
-        $("#").append(url);
-        $("#").append(yields);
-        $("#").append(image);
+        $("#recipe1").append(url).append(image);
     });
 });
